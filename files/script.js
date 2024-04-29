0a40bcc02b344d9cac66d70b99600083cc97eb89
Bulut <161609739+pleasannt@users.noreply.github.com>
2024-04-30 02:02:00 +0300

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.body.style.opacity = "1"; // Sayfa yüklendiğinde opaklık değeri 1 olacak
+    document.body.style.opacity = "1"; 
     
     document.querySelectorAll(".container").forEach(function(container) {
-        container.classList.add("show"); // Sayfa yüklendiğinde her bir container'a "show" sınıfı ekleyecek
     });
 });
